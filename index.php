de13118f08104d0ca0e14d3d7c4ca24c81a209f6
John Rainier Santiago <rainrainsantiago@gmail.com>
2021-03-09 21:29:52 +0800

    yow 3 out of 5 questions na nagagawa ko yey!

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<!DOCTYPE hmtl>
+<?php
+    include 'functions.php';
+?>
+
 <html>
     <head>
         <title>Kwek-Kwek</title>
@@ -27,7 +30,18 @@
                 </div>
                 <div id="card-temperature" class="collapse" aria-labelledby="header-card-temperature" data-parent="#accordionHead">
                     <div class="card-body">
-                        the form goes here! <br><br>
+                        <select class="form-select" id="tempchoice" aria-label="Default select example" onchange="tempChoice()">
+                            <option selected>Select Temperature</option>
+                            <option value="1">Celcius to Fahrenheit</option>
+                            <option value="2">Fahrenheit to Celcius</option>
+                        </select>
+                        <br>
+                        <form method="post">
+                            <div class="input-group mb-3">
+                                <input type="text" id="tempForm" class="form-control" placeholder="" name="" aria-describedby="button-addon2" required>
+                                <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Submit</button>
+                            </div>
+                        </form>
                         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#buttonTemperature">Show Result</button>
                     </div>
                 </div>
@@ -44,7 +58,57 @@
                 </div>
                 <div id="card-BMI" class="collapse" aria-labelledby="header-card-BMI" data-parent="#accordionHead">
                     <div class="card-body">
-                        the form goes here! <br><br>
+                        <!-- Tables for information about BMI-->
+                        <div class="table-responsive-md">
+                                <table class="table table-info table-striped table-hover" style="text-align: center;">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                            <th scope="col">BMI</th>
+                                            <th scope="col">Category</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td scope="row"> < 16.0 </td>
+                                            <td>Severely Underweighted</td>
+                                        </tr>
+                                        <tr>
+                                            <td scope="row">16.0 - 18.4</td>
+                                            <td>Underweight</td>
+                                        </tr>
+                                        <tr>
+                                            <td scope="row">18.5 - 24.9</d>
+                                            <td>Normal</td>
+                                        </tr>
+                                        <tr>
+                                            <td scope="row">25.0 - 29.9</td>
+                                            <td>Overweight</td>
+                                        </tr>
+                                        <tr>
+                                            <td scope="row">30.0 - 34.9</td>
+                                            <td>Moderately Obese</td>
+                                        </tr>
+                                        <tr>
+                                            <td scope="row">35.0 - 39.9</td>
+                                            <td>Severely Obese</td>
+                                        </tr>
+                                        <tr>
+                                            <td scope="row"> > 40.0</td>
+                                            <td>Morbidly Obese</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        <form method="post">
+                            <div class="input-group mb-3">
+                                <input type="text" id="bmiFormWeight" class="form-control" placeholder="Weight:" name="Weight" aria-describedby="button-addon3" required>
+                                <span class="input-group-text">kilogram</span>
+                                <input type="text" id="bmiFormHeight" class="form-control" placeholder="Height:" name="Height" aria-describedby="button-addon3" required>
+                                <span class="input-group-text">meters</span>
+                                <button class="btn btn-outline-secondary" type="submit" id="button-addon3">Submit</button>
+                            </div>
+                        </form>
                         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#buttonBMI">Show Results</button>
                     </div>
                 </div>
@@ -61,7 +125,31 @@
                 </div>
                 <div id="card-distance" class="collapse" aria-labelledby="header-card-distance" data-parent="#accordionHead">
                     <div class="card-body">
-                        the form goes here! <br><br>
+                        <form method="post">
+                            <div class="input-group">
+                            <span class="input-group-text">From</span>
+                                <select class="form-select" id="convertspeedselectFrom" onchange="speedChoice()">
+                                    <option selected>Select Unit: </option>
+                                    <option value="1">Miles Per Hour</option>
+                                    <option value="2">Foot Per Second</option>
+                                    <option value="3">Meter Per Second</option>
+                                    <option value="4">Kilometer Per Hour</option>
+                                </select>
+                                <input type="text" class="form-control" id="FromSpeedForm" placeholder="" name="" aria-describedby="button-select-speed">
+                            </div>
+                            <br>    
+                            <div class="input-group">
+                                <span class="input-group-text">To</span>
+                                <select class="form-select" id="convertspeedselectTo" onchange="speedChoice()">    
+                                    <option selected>Select Unit: </option>
+                                    <option value="5">Miles Per Hour</option>
+                                    <option value="6">Foot Per Second</option>
+                                    <option value="7">Meter Per Second</option>
+                                    <option value="8">Kilometer Per Hour</option>                                   
+                                </select>
+                                <button class="btn btn-outline-secondary" type="submit" id="button-select-speed">submit</button>
+                            </div>
+                        </form>
                         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#buttonDistance">Show Results</button>
                     </div>
                 </div>
@@ -108,8 +196,17 @@
         <div class="modal fade" id="buttonTemperature" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog modal-xl">
                 <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Result for temperature</h5>
+                    </div>
                     <div class="modal-body">
-                        <p>results for temperature</p>
+                        <div class="alert alert-primary" role="alert">
+                            <h4 class="alert-heading">
+                                <?php
+                                    Temperature_run();                     
+                                ?>
+                            </h4>
+                        </div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -122,8 +219,17 @@
         <div class="modal fade" id="buttonBMI" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog modal-xl">
                 <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Result for BMI</h5>
+                    </div>
                     <div class="modal-body">
-                        <p>results for BMI</p>
+                        <div class="alert alert-primary" role="alert">
+                            <h4 class="alert-heading">
+                                <?php
+                                    BMI_run();                  
+                                ?>
+                            </h4>
+                        </div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -136,8 +242,17 @@
         <div class="modal fade" id="buttonDistance" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog modal-xl">
                 <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Result for Distance</h5>
+                    </div>
                     <div class="modal-body">
-                        <p>results for distance</p>
+                        <div class="alert alert-primary" role="alert">
+                            <h4 class="alert-heading">
+                                <?php
+                                    Distance_run();
+                                ?>
+                            </h4>
+                        </div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -151,8 +266,15 @@
         <div class="modal fade" id="buttonVolume" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog modal-xl">
                 <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Result for Volume</h5>
+                    </div>
                     <div class="modal-body">
-                        <p>results for volume</p>
+                        <div class="alert alert-primary" role="alert">
+                            <h4 class="alert-heading">
+                                    
+                            </h4>
+                        </div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -166,8 +288,15 @@
         <div class="modal fade" id="buttonPayroll" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog modal-xl">
                 <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Result for Payroll</h5>
+                    </div>
                     <div class="modal-body">
-                        <p>results for payroll</p>
+                        <div class="alert alert-primary" role="alert">
+                            <h4 class="alert-heading">
+                                    
+                            </h4>
+                        </div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -178,6 +307,7 @@
 
 
         <!-- Script -->
+        <script src="app.js"></script>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
